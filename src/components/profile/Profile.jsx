30f96012945c8265df09039637c1e8b8192dd281
EvgenS-it <evgens.it101@gmail.com>
2024-07-22 19:19:47 +0100

    profile files added

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,12 @@
+import css from './Profile.module.css';
+
+const Profile = () => {
+  return (
+    <div>
+      <h2 className={css.test}>Tacos</h2>
+      <p>Price: 999 credits</p>
+    </div>
+  );
+};
+
+export default Profile;
