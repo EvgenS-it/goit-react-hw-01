e101dfa0181e18b8f3a223c48777de6b706b34cb
EvgenS-it <evgens.it101@gmail.com>
2024-07-23 21:44:21 +0100

    task-1 done

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,35 +1,40 @@
 import css from './Profile.module.css';
+import PropTypes from 'prop-types';
 
-const Profile = () => {
+const Profile = ({ name, tag, location, image, stats }) => {
   return (
     <div className={css.profileCard}>
-      <div className="mainInfoContainer">
-        <img
-          className={css.userImg}
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-        />
-        <p className="mainInfoText">Petra Marica</p>
-        <p className="mainInfoText">@pmarica</p>
-        <p className="mainInfoText">Salvador, Brasil</p>
+      <div className={css.mainInfoContainer}>
+        <img className={css.userImg} src={image} alt="User avatar" />
+        <p className={css.mainInfoName}>{name}</p>
+        <p className={css.mainInfoText}>{tag}</p>
+        <p className={css.mainInfoText}>{location}</p>
       </div>
 
-      <ul className="userInfoList">
-        <li className="infoItem">
-          <span className="infoItemText">Followers</span>
-          <span className="infoItemText">1000</span>
+      <ul className={css.userInfoList}>
+        <li className={css.infoItem}>
+          <span className={css.infoItemText}>Followers</span>
+          <span className={css.infoItemNumber}>{stats.followers}</span>
         </li>
-        <li className="infoItem">
-          <span className="infoItemText">Views</span>
-          <span className="infoItemText">2000</span>
+        <li className={css.infoItem}>
+          <span className={css.infoItemText}>Views</span>
+          <span className={css.infoItemNumber}>{stats.views}</span>
         </li>
-        <li className="infoItem">
-          <span className="infoItemText">Likes</span>
-          <span className="infoItemText">3000</span>
+        <li className={css.infoItem}>
+          <span className={css.infoItemText}>Likes</span>
+          <span className={css.infoItemNumber}>{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  image: PropTypes.string,
+  stats: PropTypes.object,
 };
 
 export default Profile;
